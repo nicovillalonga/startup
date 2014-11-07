6d0eed74befd9a3bf6e31fea7c207758023a4514
nicolasv <nicovillalonga@hotmail.com>
2014-11-06 21:14:47 -0300

    correccion topic 1

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
-    $('#divHidden').fadeIn(2000);
+    $('.divHidden').fadeIn(2000);
     
     $('.alias').focus();
     
     $('#btn').click(function(){
-        $.getJSON('http://bootcamp.aws.af.cm/welcome/Nicolas', function(response){
-            $('.alias').val(response.response);
-        }).fail(function(){
-            $('.alias').val('Error fetching data').css("background-color", "red");
-        });
+        var nombre = $('.alias').val();
+        if (nombre !== "")
+        {
+            $.getJSON('http://botcamp.aws.af.cm/welcome/' + nombre, function(response){
+                $('.response').append('<p>'+ response.response +'</p>');
+                }).fail(function(){
+                    var msg = 'Error fetching data';
+                    $('.response').append('<p>'+ msg +'</p>').css("background-color", "red");
+                    
+                    //$('.alias').val('Error fetching data').css("background-color", "red");
+                });
+        }
+        else
+            alert('ingrese su nombre');
     });
     
     $.ajax({
@@ -16,7 +25,8 @@ $(document).ready(function(){
         type: "get"
     }).success(function(data){
             console.log(data);
-            $.each(data, function(){
+            $.each(data.statuses, function(i, val){
+                /*
                 $.each(this, function(){
                     $('.sideBar').append('<div class="tweet">' 
                                          + '<label>' + this.text + '</label><br>' 
@@ -25,6 +35,14 @@ $(document).ready(function(){
                                          + '</div>');
                 });
             });
+        */
+                        $('.sideBar').append('<div class="tweet">' 
+                                                 + '<label>' + val.text + '</label><br>' 
+                                                 + '<label>' + this.created_at + '</label><br>' 
+                                                 + '<a href="' + this.user.profile_image_url + '">http://pbs.twimg.com/profile...jpeg</a><br><br>'
+                                                 + '</div>');
+               
+                    });
     }).fail(function(){
         alert('error');
     });
